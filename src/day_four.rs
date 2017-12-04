3cc9473fe1b93336cfb3accb1bf90f0ad53cd88c
Lisa Vitolo <syn.shainer@gmail.com>
2017-12-04 21:16:34 +0000

    Day 4, part 1 and 2.

---
+++ src/day_four.rs
@@ -0,0 +1,69 @@
+use std::collections::HashSet;
+use std::io::prelude::*;
+use std::iter::FromIterator;
+use std::fs::File;
+
+fn read_input(path : &str) -> String {
+    let mut f = File::open(path).expect("File not found.");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Error reading the file contents.");
+    contents
+}
+
+fn sort_word_chars(word : &str) -> String {
+    let mut chars : Vec<char> = word.chars().collect();
+    chars.sort_by(|a, b| b.cmp(a));
+    String::from_iter(chars)
+}
+
+pub fn day_four_part_two() {
+    let contents = read_input("data/day_four.txt");
+    let mut valid_count = 0;
+
+    for passphrase in contents.split('\n') {
+        let mut sorted_words : HashSet<String> = HashSet::new();
+        let mut is_good = true;
+
+        for word in passphrase.split(' ') {
+            let sorted_word = sort_word_chars(word);
+
+            if sorted_words.contains(&sorted_word) {
+                is_good = false;
+                break;
+            }
+
+            sorted_words.insert(sorted_word);
+        }
+
+        if is_good && sorted_words.len() > 1 {
+            valid_count += 1;
+        }
+    }
+
+    println!("Day 4 part 2. Count of valid passphrases is {}.", valid_count);
+}
+
+pub fn day_four() {
+    let contents = read_input("data/day_four.txt");
+    let mut valid_count = 0;
+
+    for passphrase in contents.split('\n') {
+        let mut words = HashSet::new();
+        let mut is_good = true;
+
+        for word in passphrase.split(' ') {
+            if words.contains(word) {
+                is_good = false;
+                break;
+            }
+            words.insert(word);
+        }
+
+        if is_good && words.len() > 1 {
+            valid_count += 1;
+        }
+    }
+
+    println!("Day 4 part 1. Count of valid passphrases is {}", valid_count);
+}
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,20 @@ use day_two::*;
 mod day_three;
 use day_three::*;
 
+mod day_four;
+use day_four::*;
+
 // TODO: make the main intelligent, i.e. able to get the day number from command line
 // and only run the corresponding function(s). For now this is good enough.
 fn main() {
-    // day_one(1);
-    // day_one(1007);
-    // day_two();
-    // day_two_part_two();
+    day_one(1);
+    day_one(1007);
+    day_two();
+    day_two_part_two();
 
-    //day_three();
+    day_three();
     day_three_part_two();
+
+    day_four();
+    day_four_part_two();
 }
