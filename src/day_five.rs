18dd717803eabefffc97d9997256304ea746ad95
Lisa Vitolo <syn.shainer@gmail.com>
2017-12-09 11:08:54 +0000

    Day 5 part 2 solution, general cleanup.

--- src/day_five.rs
+++ src/day_five.rs
@@ -1,29 +1,29 @@
 use utils::read_input;
 
 pub fn day_five() {
-    let mut lines : Vec<String> = Vec::new();
+    let mut lines: Vec<i32> = Vec::new();
 
     for l in read_input("data/day_five.txt").split('\n') {
-        lines.push(l.to_owned());
-    }
-
-    let mut current_index : i32 = 0;
-    let mut steps = 0;
-
-    loop {
-        if lines[current_index as usize].len() == 0 {
+        if l.is_empty() {
             break;
         }
 
-        let num : i32 = lines[current_index as usize].parse().expect("Not a number");
-        let next_index : i32 = num + current_index;
+        lines.push(l.parse().expect("Not a number!"));
+    }
+
+    let mut current_index: i32 = 0;
+    let mut steps = 0;
+
+    loop {
+        let num: i32 = lines[current_index as usize];
+        let next_index: i32 = num + current_index;
         steps += 1;
 
         if next_index < 0 || next_index >= lines.len() as i32 {
             break;
         }
 
-        lines[current_index as usize] = (num + 1).to_string();
+        lines[current_index as usize] = num + 1;
         current_index = next_index;
     }
 
@@ -31,38 +31,39 @@ pub fn day_five() {
 }
 
 pub fn day_five_part_two() {
-    let mut lines : Vec<String> = Vec::new();
+    let mut lines: Vec<i32> = Vec::new();
 
     for l in read_input("data/day_five.txt").split('\n') {
-        lines.push(l.to_owned());
+        if l.is_empty() {
+            continue;
+        }
+
+        lines.push(l.parse().expect("Not a number!"));
     }
 
-    let mut current_index : i32 = 0;
+    let mut current_index: i32 = 0;
     let mut steps = 0;
 
     loop {
-        if lines[current_index as usize].len() == 0 {
-            break;
-        }
-
-        let num : i32 = lines[current_index as usize].parse().expect("Not a number");
-        let next_index : i32 = num + current_index;
-        println!("Current index is {} and num is {}, so I jump to {}", current_index, num, next_index);
+        let num: i32 = lines[current_index as usize];
+        let next_index: i32 = num + current_index;
         steps += 1;
 
         if next_index < 0 || next_index >= lines.len() as i32 {
             break;
         }
 
-        let mut offset : i32 = 1;
+        let mut offset: i32 = 1;
         if num >= 3 {
             offset = -1;
         }
 
-        println!("{} becomes {}", lines[current_index as usize], (num+offset));
-        lines[current_index as usize] = (num + offset).to_string();
+        lines[current_index as usize] = num + offset;
         current_index = next_index;
     }
 
-    println!("Day 5 part 2. It takes {} steps to leave the memory.", steps);
+    println!(
+        "Day 5 part 2. It takes {} steps to leave the memory.",
+        steps
+    );
 }
