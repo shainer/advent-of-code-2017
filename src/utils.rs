af799bde95358cec84cb7546e35a941782d73d93
Lisa Vitolo <syn.shainer@gmail.com>
2017-12-09 11:09:13 +0000

    Format utils.

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,11 @@
 use std::io::prelude::*;
 use std::fs::File;
 
-pub fn read_input(path : &str) -> String {
+pub fn read_input(path: &str) -> String {
     let mut f = File::open(path).expect("File not found.");
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("Error reading the file contents.");
+    f.read_to_string(&mut contents).expect(
+        "Error reading the file contents.",
+    );
     contents
 }
